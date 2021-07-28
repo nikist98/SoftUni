11cc6de03480a76f98d30bfaa8ffeb9b784593ef
Atanas Stanchev <atanas.stanchev@opencode.com>
2021-07-28 21:30:43 +0300

    Add exercices

---
+++ Concanate-data.js
@@ -0,0 +1,10 @@
+function cancanatedata(input){
+let name = input[0]
+let lastname = input [1]
+let age = input [2]
+let town = input [3]
+
+console.log(`You are ${name} ${lastname}, a ${age}-years old person from ${town}.`)
+
+}
+cancanatedata([''])
---
+++ Square.js
@@ -0,0 +1,6 @@
+function square(input) {
+ let a = parseFloat(input[0]);
+ console.log(a+a)
+
+}
+square(["37.6"])
